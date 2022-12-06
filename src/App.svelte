ef5579687e45b2edb5b5da68f3b150ad083cf05f
unknown <Tomas.Wandahl@live.se>
2022-12-06 16:27:37 +0100

    initial commit, installed svelte, deleted boilerplate code

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Button from './components/Button.svelte'
+</script>
+
+<main>
+    <div class="content">
+        <h1>Tomas + Svelte = Sant</h1>
+        <div class="card">
+            <Button />
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
